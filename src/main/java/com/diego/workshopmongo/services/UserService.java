dd0eb96ecf8420010054dead72e4a547981ef3a6
Diego Rodrigues <diego_r.g@hotmail.com>
2021-02-04 16:26:56 +0100

    Conectando ao mongodb com repository e service

---
+++ src/main/java/com/diego/workshopmongo/services/UserService.java
@@ -0,0 +1,21 @@
+package com.diego.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.diego.workshopmongo.domain.User;
+import com.diego.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll(){
+		return repo.findAll();
+	}
+
+}
