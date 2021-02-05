e2a1376427e0a970b5c6cd331c93c92d1a7c83cc
Diego Rodrigues <diego_r.g@hotmail.com>
2021-02-05 16:19:59 +0100

    Obtendo um post por ID

---
+++ src/main/java/com/diego/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.diego.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.diego.workshopmongo.domain.Post;
+import com.diego.workshopmongo.repository.PostRepository;
+import com.diego.workshopmongo.services.exception.ObjectNotFoundEception;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundEception("Objeto nao encontrado"));
+	}
+}
